da7f6d26c9ca07072cdffb51061c1b51eb255a91
heikogeorg.menzler <heikogeorg.menzler@stud.uni-goettingen.de>
2020-07-04 12:07:14 +0200

    Added simple regression.

--- fitting/linear_regression.py
+++ fitting/linear_regression.py
@@ -15,11 +15,28 @@ def linear_regression(data, err):
     print(data)
     beta0 = get_start_values(data)
     model = Model(linear_model)
-    rdata = RealData(data[:, 0], data[:, 1], sx=err[0])  # TODO: check different errors
+    rdata = RealData(data[:, 0], data[:, 1], sx=err[0])
+    # TODO: check different errors
     fit = ODR(rdata, model, beta0=beta0)
     output = fit.run()
     print(output.beta)
     return output
+
+
+def simple_regression(data):
+    """Calculate linear regression for errorless data."""
+    n = len(data)
+    x_sum = sum(data[:, 0])
+    y_sum = sum(data[:, 1])
+    xy_sum = sum(data[:, 0] * data[:, 1])
+    x_sq_sum = sum(data[:, 0] * data[:, 0])
+    denom = n * x_sq_sum - x_sum * x_sum
+    m = (n * xy_sum - x_sum * y_sum) / denom
+    b = (x_sq_sum * y_sum - x_sum * xy_sum) / denom
+    dev_sum = sum((data[:, 1] - b - m * data[:, 0]) ** 2)
+    sig_m = (n * dev_sum) / (n-2) / denom
+    sig_b = (x_sq_sum * dev_sum) / (n-2) / denom
+    return m, b, sig_m, sig_b
 
 
 def get_start_values(data):
